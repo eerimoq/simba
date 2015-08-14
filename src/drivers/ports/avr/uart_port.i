d2aa2112d52de3401234897331068fd86e059347
erik <erik.moqvist@gmail.com>
2015-08-14 23:43:45 +0200

    funduino port started and does not work

--- src/drivers/ports/avr/uart_port.i
+++ src/drivers/ports/avr/uart_port.i
@@ -20,6 +20,75 @@
 
 #include <avr/interrupt.h>
 
+#if defined(__AVR_ATmega2560__)
+
+static int uart_port_start(struct uart_driver_t *drv_p)
+{
+    uint16_t baudrate = (F_CPU / 16 / drv_p->baudrate - 1);
+
+    UBRR0H = (baudrate >> 8);
+    UBRR0L = baudrate;
+    UCSR0B = (_BV(RXCIE0) | _BV(TXCIE0) | _BV(RXEN0) | _BV(TXEN0));
+    UCSR0C = (_BV(UCSZ00) | _BV(UCSZ01));
+
+    drv_p->dev_p->drv_p = drv_p;
+
+    return (0);
+}
+
+static int uart_port_stop(struct uart_driver_t *drv_p)
+{
+    UCSR0B = 0;
+    UCSR0C = 0;
+
+    drv_p->dev_p->drv_p = NULL;
+
+    return (0);
+}
+
+static ssize_t uart_port_write_cb(void *arg_p,
+                                  const void *txbuf_p,
+                                  size_t size)
+{
+    struct uart_driver_t *drv_p;
+
+    /* Initiate transfer by writing the first byte. */
+    drv_p = container_of(arg_p, struct uart_driver_t, chout);
+    drv_p->txbuf_p = txbuf_p;
+    drv_p->txsize = (size - 1);
+    drv_p->thrd_p = thrd_self();
+    UDR0 = *drv_p->txbuf_p++;
+
+    thrd_suspend(NULL);
+
+    return (size);
+}
+
+ISR(USART0_TX_vect)
+{
+    struct uart_driver_t *drv_p = uart_device[0].drv_p;
+
+    /* Write next byte or resume suspended thread. */
+    if (drv_p->txsize > 0) {
+        UDR0 = *drv_p->txbuf_p++;
+        drv_p->txsize--;
+    } else {
+        thrd_resume_irq(drv_p->thrd_p, 0);
+    }
+}
+
+ISR(USART0_RX_vect)
+{
+    struct uart_driver_t *drv_p = uart_device[0].drv_p;
+    char c;
+
+    /* Write data to input queue. */
+    c = UDR0;
+    queue_write_irq(&drv_p->chin, &c, 1);
+}
+
+#else
+
 static int uart_port_start(struct uart_driver_t *drv_p)
 {
     uint16_t baudrate = (F_CPU / 16 / drv_p->baudrate - 1);
@@ -84,3 +153,5 @@ ISR(USART_RX_vect)
     c = UDR0;
     queue_write_irq(&drv_p->chin, &c, 1);
 }
+
+#endif
---
+++ src/mcus/atmega2560/mcu.c
@@ -0,0 +1,77 @@
+/**
+ * @file mcus/atmega328p/mcu.c
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2014-2015, Erik Moqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * This file is part of the Simba project.
+ */
+
+#include "simba.h"
+
+struct pin_device_t pin_device[PIN_DEVICE_MAX] = {
+    /* PIN D */
+    { .sfr_p = &PIND, .mask = 0x04 },
+    { .sfr_p = &PIND, .mask = 0x08 },
+    { .sfr_p = &PIND, .mask = 0x10 },
+    { .sfr_p = &PIND, .mask = 0x20 },
+    { .sfr_p = &PIND, .mask = 0x40 },
+    { .sfr_p = &PIND, .mask = 0x80 },
+
+    /* PIN B */
+    { .sfr_p = &PINB, .mask = 0x01 },
+    { .sfr_p = &PINB, .mask = 0x02 },
+    { .sfr_p = &PINB, .mask = 0x04 },
+    { .sfr_p = &PINB, .mask = 0x08 },
+    { .sfr_p = &PINB, .mask = 0x10 },
+    { .sfr_p = &PINB, .mask = 0x20 },
+
+    /* PIN C */
+    { .sfr_p = &PINC, .mask = 0x01 },
+    { .sfr_p = &PINC, .mask = 0x02 },
+    { .sfr_p = &PINC, .mask = 0x04 },
+    { .sfr_p = &PINC, .mask = 0x08 },
+    { .sfr_p = &PINC, .mask = 0x10 },
+    { .sfr_p = &PINC, .mask = 0x20 },
+};
+
+struct exti_device_t exti_device[EXTI_DEVICE_MAX] = {
+    { .drv_p = NULL, .pin_p = &pin_d2_dev, .id = 0 },
+    { .drv_p = NULL, .pin_p = &pin_d3_dev, .id = 1 }
+};
+
+struct spi_device_t spi_device[SPI_DEVICE_MAX] = {
+    {
+        .drv_p = NULL,
+        .mosi_p = &pin_d11_dev,
+        .miso_p = &pin_d12_dev,
+        .sck_p = &pin_d13_dev,
+        .sem = { .count = 1, .head_p = NULL }
+    }
+};
+
+struct uart_device_t uart_device[UART_DEVICE_MAX] = {
+    { .drv_p = NULL }
+};
+
+struct pwm_device_t pwm_device[PWM_DEVICE_MAX] = {
+    { .index = 0, .pin_dev_p = &pin_device[7] }, /* timer1, pin D9 */
+    { .index = 1, .pin_dev_p = &pin_device[8] }, /* timer1, pin D10 */
+    { .index = 2, .pin_dev_p = &pin_device[9] }, /* timer2, pin D11 */
+    { .index = 3, .pin_dev_p = &pin_device[1] }  /* timer2, pin D3 */
+};
+
+struct adc_device_t adc_device[ADC_DEVICE_MAX] = {
+    { .jobs = { .head_p = NULL, .tail_p = NULL } },
+};
