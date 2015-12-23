839c7122e7db7e0f96c372192b904a5a7276a3c2
erik <erik.moqvist@gmail.com>
2015-12-23 09:18:56 +0100

    Use read the docs theme for the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -122,25 +122,27 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = "alabaster"
+html_theme = 'sphinx_rtd_theme'
 #html_theme = 'pydoctheme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {'collapsiblesidebar': True}
-html_theme_options = {
-    'page_width': '70%',
-    'github_banner': True,
-    'github_user': 'eerimoq',
-    'github_repo': 'simba',
-    'logo': "Simba-Logo.png"
-}
+
+# alabaster
+#html_theme_options = {
+#    'page_width': '70%',
+#    'github_banner': True,
+#    'github_user': 'eerimoq',
+#    'github_repo': 'simba',
+#    'logo': "Simba-Logo.png"
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_path = [alabaster.get_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
