3861250b96238f3029aa63ed767185c1b6405660
Erik Moqvist <erik.moqvist@gmail.com>
2016-04-24 23:39:24 +0200

    Renamed rust to rost

--- make/app.mk
+++ make/app.mk
@@ -195,9 +195,9 @@ endef
 $(foreach file,$(CSRC),$(eval $(call COMPILE_template,$(file))))
 
 RUSTLIB = rustlib
-RUST_ROOT ?= $(SIMBA_ROOT)/rust/rust
+RUST_ROOT ?= $(SIMBA_ROOT)/rost/rust
 RUST_TARGET ?=
-RUST_SIMBA_RS = $(SIMBA_ROOT)/rust/src/simba.rs
+RUST_SIMBA_RS = $(SIMBA_ROOT)/rost/src/simba.rs
 RUST_LIBSIMBA = libsimba.a
 RUST_LIBSIMBA_SRC = \
 	lib.rs \
@@ -241,9 +241,9 @@ else
 	cp $(@:$(RUSTLIB)/%.a=%.rlib) $@
 endif
 
-$(RUSTLIB)/$(RUST_LIBSIMBA): $(RUST_SIMBA_RS) $(RUST_LIBSIMBA_SRC:%=$(SIMBA_ROOT)/rust/src/%)
+$(RUSTLIB)/$(RUST_LIBSIMBA): $(RUST_SIMBA_RS) $(RUST_LIBSIMBA_SRC:%=$(SIMBA_ROOT)/rost/src/%)
 	@echo "Compiling RUST $@..."
-	rustc $(RSFLAGS) $(SIMBA_ROOT)/rust/src/lib.rs --crate-name simba --out-dir .
+	rustc $(RSFLAGS) $(SIMBA_ROOT)/rost/src/lib.rs --crate-name simba --out-dir .
 	cp libsimba.rlib $@
 
 -include $(DEPSDIR)/simba.rs.dep
