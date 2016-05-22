ee6934dd44c38ae9aa2ce5d78f9863202e2c9947
Erik Moqvist <erik.moqvist@gmail.com>
2016-05-22 19:38:06 +0200

    Configuration file for thread test suite.

---
+++ tst/kernel/thrd/config.h
@@ -0,0 +1,56 @@
+/**
+ * @file config.h
+ * @version 0.5.0
+ *
+ * @section License
+ * Copyright (C) 2014-2016, Erik Moqvist
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * This file is part of the Simba project.
+ */
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/**
+ * Build with assert.
+ */
+#ifndef CONFIG_ASSERT
+#    define CONFIG_ASSERT               1
+#endif
+
+/**
+ * Stack profiling.
+ */
+#ifndef CONFIG_PROFILE_STACK
+#    define CONFIG_PROFILE_STACK        1
+#endif
+
+/**
+ * Use a preemptive scheduler.
+ */
+#ifndef CONFIG_PREEMPTIVE_SCHEDULER
+#    if defined(ARCH_AVR) || defined(ARCH_ARM)
+#        define CONFIG_PREEMPTIVE_SCHEDULER 1
+#    else
+#        define CONFIG_PREEMPTIVE_SCHEDULER 0
+#    endif
+#endif
+
+/**
+ * Start the monitor thread.
+ */
+#ifndef CONFIG_MONITOR_THREAD
+#    define CONFIG_MONITOR_THREAD       1
+#endif
+
+#endif
