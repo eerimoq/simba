594a80715d9d0574083714d8e74884b828ea68f9
Erik Moqvist <erik.moqvist@gmail.com>
2016-06-08 18:59:46 +0200

    Documentation and comments.

--- doc/api-reference/kernel/log.rst
+++ doc/api-reference/kernel/log.rst
@@ -4,6 +4,24 @@
 .. module:: log
    :synopsis: Logging.
 
+The logging module consists of log objects and log handlers. A log
+object filters log entries and a log handler writes log entries to an
+output channel.
+
+A log object called "log" and a log handler writing to standard output
+are created during the log module initialization. The log handler can
+be replaced by calling ``log_set_default_handler_output_channel()``.
+
+Normally one log object is created for each subsystem in an
+application. This gives the user the power to control which parts of
+the system to debug and/or monitor at runtime.
+
+Sometimes it's useful to write log entries to multiple channels. This
+is possible by creating and adding another log handler to the log
+module.
+
+----------------------------------------------
+
 Source code: :github-blob:`src/kernel/kernel/log.h`
 
 Test code: :github-blob:`tst/kernel/log/main.c`
--- src/kernel/kernel/log.h
+++ src/kernel/kernel/log.h
@@ -67,63 +67,6 @@ struct log_object_t {
 int log_module_init(void);
 
 /**
- * Add given log handler.
- *
- * @param[in] handler_p Handler to add.
- *
- * @return zero(0) or negative error code.
- */
-int log_add_handler(struct log_handler_t *handler_p);
-
-/**
- * Remove given log handler.
- *
- * @param[in] handler_p Handler to remove.
- *
- * @return zero(0) or negative error code.
- */
-int log_remove_handler(struct log_handler_t *handler_p);
-
-/**
- * Add given log object.
- *
- * @param[in] object_p Object to add.
- *
- * @return zero(0) or negative error code.
- */
-int log_add_object(struct log_object_t *object_p);
-
-/**
- * Remove given log object.
- *
- * @param[in] object_p Object to remove.
- *
- * @return zero(0) or negative error code.
- */
-int log_remove_object(struct log_object_t *object_p);
-
-/**
- * Set the default output channel.
- *
- * @param[in] chout_p Channel to set as the default output
- *                    channel. May be NULL.
- *
- * @return zero(0) or negative error code.
- */
-int log_set_default_handler_output_channel(chan_t *chout_p);
-
-/**
- * Initialize given handler.
- *
- * @param[in] self_p Log handler to initialize.
- * @param[in] chout_p Output handler.
- *
- * @return zero(0) or negative error code.
- */
-int log_handler_init(struct log_handler_t *self_p,
-                     chan_t *chout_p);
-
-/**
  * Initialize given log object with given name and mask.
  *
  * @param[in] self_p Log object to initialize.
@@ -137,13 +80,13 @@ int log_object_init(struct log_object_t *self_p,
                     char mask);
 
 /**
- * Format message and print it if given log level is set in the log
- * object mask.
+ * Check if given log level is set in the log object mask. If so,
+ * format a log entry and write it to all log handlers.
  *
- * ``self_p`` may be NULL, and in that case the current thread log
+ * ``self_p`` may be NULL, and in that case the current thread's log
  * mask is used instead of the log object mask.
  *
- * @param[in] self_p Log object, or NULL to use the thread log mask.
+ * @param[in] self_p Log object, or NULL to use the thread's log mask.
  * @param[in] level Log level.
  * @param[in] fmt_p Log format string.
  * @param[in] ... Variable argument list.
@@ -154,5 +97,66 @@ int log_object_print(struct log_object_t *self_p,
                      int level,
                      const char *fmt_p,
                      ...);
+
+/**
+ * Initialize given log handler with given output channel.
+ *
+ * @param[in] self_p Log handler to initialize.
+ * @param[in] chout_p Output handler.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_handler_init(struct log_handler_t *self_p,
+                     chan_t *chout_p);
+
+/**
+ * Add given log handler to the list of log handlers. Log entries will
+ * be written to all log handlers in the list.
+ *
+ * @param[in] handler_p Log handler to add.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_add_handler(struct log_handler_t *handler_p);
+
+/**
+ * Remove given log handler from the list of log handlers.
+ *
+ * @param[in] handler_p Log handler to remove.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_remove_handler(struct log_handler_t *handler_p);
+
+/**
+ * Add given log object to the list of log objects. There are file
+ * system commands to list all log objects in the list and also modify
+ * their log mask.
+ *
+ * @param[in] object_p Log object to add.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_add_object(struct log_object_t *object_p);
+
+/**
+ * Remove given log object from the list of log objects.
+ *
+ * @param[in] object_p Object to remove.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_remove_object(struct log_object_t *object_p);
+
+/**
+ * Set the output channel of the default log handler.
+ *
+ * @param[in] chout_p Channel to set as the default output
+ *                    channel. May be NULL if no output should be
+ *                    written.
+ *
+ * @return zero(0) or negative error code.
+ */
+int log_set_default_handler_output_channel(chan_t *chout_p);
 
 #endif
