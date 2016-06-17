d049b008f792580479d92c3f9195e040cabefb7b
Erik Moqvist <erik.moqvist@gmail.com>
2016-06-17 07:49:12 +0200

    lwIP configuration and documentation.

--- doc/user-guide/configuration.rst
+++ doc/user-guide/configuration.rst
@@ -36,3 +36,156 @@ Variables
 +-------------------------------+-------------------------------------------+
 |  CONFIG_MONITOR_THREAD        | Start the monitor thread.                 |
 +-------------------------------+-------------------------------------------+
+
+lwIP
+^^^^
+
+Use ``config.h`` to fully configure lwIP and all of its modules. You
+do not need to define every option that lwIP provides; if you do not
+define an option, a default value will be used. Therefore, your
+``config.h`` provides a way to override much of the behavior of lwIP.
+
+By default `Simba` overrides a few of the variables in
+:github-blob:`src/inet/lwipopts.h`.
+
+Module support (Code size)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enabling and disabling modules
+""""""""""""""""""""""""""""""
+
+You can tune your code size by only compiling the features you really
+need. The following is a list of what gets compiled in "out of the
+box" with lwIP.
+
+Default inclusions:
+
+- ARP (``LWIP_ARP``)
+- IP and fragmentation (``IP_FRAG``) and reassembly (``IP_REASSEMBLY``)
+- Raw IP PCB support (``LWIP_RAW``)
+- UDP (``LWIP_UDP``) and UDP-Lite (``LWIP_UDPLITE``)
+- TCP (``LWIP_TCP``) -- this is a big one!
+- Statistics (``LWIP_STATS``)
+
+Default exclusions:
+
+- DHCP (``LWIP_DHCP``)
+- AUTOIP (``LWIP_AUTOIP``)
+- SNMP (``LWIP_SNMP``)
+- IGMP (``LWIP_IGMP``)
+- PPP (``PPP_SUPPORT``)
+
+If you would like to change this, then you just need to set the
+options listed below. For example, if you would like to disable UDP
+and enable DHCP, the following ``config.h`` file would do it:
+
+.. code:: c
+
+   /* Disable UDP */
+   #define LWIP_UDP 0
+
+   /* Enable DHCP */
+   #define LWIP_DHCP 1
+
+Memory management (RAM usage)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Memory pools
+""""""""""""
+
+In an embedded environment, memory pools make for fast and efficient
+memory allocation. lwIP provides a flexible way to manage memory pool
+sizes and organization.
+
+lwIP reserves a fixed-size static chunk of memory in the data segment,
+which is subdivided into the various pools that lwip uses for the
+various data structures. For example, there is a pool just for struct
+tcp_pcb's, and another pool just for struct udp_pcb's. Each pool can
+be configured to hold a fixed number of data structures; this number
+can be changed in the ``config.h`` file by changing the various
+``MEMP_NUM_*`` values. For example, ``MEMP_NUM_TCP_PCB`` and
+``MEMP_NUM_UDP_PCB`` control the maximum number of tcp_pcb and udp_pcb
+structures that can be active in the system at any given time.
+
+It is also possible to create custom memory pools in addition to the
+standard ones provided by lwIP.
+
+Dynamic allocation: mem_malloc
+""""""""""""""""""""""""""""""
+
+lwIP uses a custom function mem_malloc for all dynamic allocation;
+therefore, it is easy to change how lwIP uses its RAM. There are three
+possibilities provided out-of-the-box:
+
+1. (default) lwIP's custom heap-based mem_malloc. By default, lwIP
+   uses a statically-allocated chunk of memory like a heap for all
+   memory operations. Use ``MEM_SIZE`` to change the size of the lwIP
+   heap.
+
+2. C standard library malloc and free. If you wish to have lwIP use
+   the standard library functions provided by your
+   compiler/architecture, then define the option ``MEM_LIBC_MALLOC``.
+
+3. Memory pools. lwIP can also emulate dynamic allocation using custom
+   memory pools (see that chapter for more information). This involves
+   the options ``MEM_USE_POOLS`` and ``MEMP_USE_CUSTOM_POOLS`` and a
+   new custom file ``lwippools.h``.
+
+Understanding/changing memory usage
+"""""""""""""""""""""""""""""""""""
+
+lwIP uses memory for:
+
+- code (depending on your system, may use ROM instead of RAM)
+
+- statically allocated variables (some initialized, some not
+  initialized)
+
+- task stack
+
+- dynamically allocated memory
+
+     - heap
+     - memp pools
+
+Unless you use a C library heap implementation (by defining
+``MEM_LIBC_MALLOC`` to 1), dynamically allocated memory must be statically
+allocated somewhere. This means you reserve a specific amount of
+memory for the heap or the memp pools from which the code dynamically
+allocates memory at runtime.
+
+The size of this heap and memp pools can be adjusted to save RAM:
+
+There are 3 types of pbufs:
+
+- REF/ROM, RAM and POOL. ``PBUF_POOL_SIZE`` * ``PBUF_POOL_BUFSIZE``
+  only refers to type POOL.
+
+- RAM pbufs are allocated in the memory defined by ``MEM_SIZE`` (this
+  memory is not used much aside from RAM pbufs) - this is the *heap*
+  and it is allocated as mem_memory.
+
+- REF/ROM pbufs as well as pcbs and some other stuff is allocated from
+  dedicated pools per structure type. The amount of structures is
+  defined by the various ``MEMP_NUM_`` defines. Together, this memory
+  is allocated as memp_memory and it *includes* the pbuf POOL.
+
+However, if you define ``MEMP_MEM_MALLOC`` to 1 in your ``config.h``,
+*every* piece of dynamically allocated memory will come from the heap
+(the size of which is defined by ``MEM_SIZE``). If you then even
+define ``MEM_LIBC_MALLOC`` to 1, too, lwIP doesn't need extra memory
+for dynamically allocated memory but only uses the C library heap
+instead. However, you then have to make sure that this heap is big
+enough to run your application.
+
+To tweak the various ``MEMP_NUM_`` defines, define ``LWIP_STATS=1``
+and ``LWIP_STATS_DISPLAY=1`` and call `stats_display()` to see how
+many entries of each pool are used (or have a look at the global
+variable ``lwip_stats`` instead).
+
+Fine-tuning even more
+~~~~~~~~~~~~~~~~~~~~~
+
+To see the options that you can set, open
+``lwip/src/include/lwip/opt.h``. This file is fully commented and
+explains how many of the options are used.
--- src/inet/lwipopts.h
+++ src/inet/lwipopts.h
@@ -21,99 +21,265 @@
 #ifndef __LWIPOPTS_H__
 #define __LWIPOPTS_H__
 
-#define NO_SYS                      0
-
-#define SYS_LIGHTWEIGHT_PROT        1
-
-#define LWIP_RAW                    0
-
-#define TCPIP_MBOX_SIZE             8
-#define DEFAULT_TCP_RECVMBOX_SIZE   8
-#define DEFAULT_UDP_RECVMBOX_SIZE   8
-#define DEFAULT_RAW_RECVMBOX_SIZE   8
-#define DEFAULT_ACCEPTMBOX_SIZE     8
-
-#define TCPIP_THREAD_NAME           "tcpip"
-#define TCPIP_THREAD_STACKSIZE      1024
-#define TCPIP_THREAD_PRIO           0
-
-#define MEM_SIZE                    8000
-
-#define MEMP_NUM_SYS_TIMEOUT        16
-
-#define MEM_ALIGNMENT               4
-
-#define PBUF_POOL_SIZE              8
-#define MEMP_NUM_TCP_PCB_LISTEN     4
-#define MEMP_NUM_TCP_PCB            4
-#define MEMP_NUM_UDP_PCB            4
-#define MEMP_NUM_PBUF               8
-#define MEMP_NUM_NETBUF             8
-
-#define TCP_QUEUE_OOSEQ             0
-#define TCP_OVERSIZE                0
-
-#define LWIP_DHCP                   1
-#define LWIP_DNS                    1
-
 #include "stdlib.h"
-#define LWIP_IGMP                   1
-#define LWIP_RAND()                 rand()
 
+/* These defines should not be changed by the user. The Simba port of
+ * the lwIP stack requires them to have given values. */
+#define NO_SYS                      0
+#define SYS_LIGHTWEIGHT_PROT        1
 #define LWIP_SOCKET                 0
-
 #define LWIP_COMPAT_SOCKETS         0
 #define LWIP_POSIX_SOCKETS_IO_NAMES 0
-#define LWIP_SO_RCVTIMEO            1
-#define LWIP_TCP_KEEPALIVE          1
-
-#define LWIP_NOASSERT               1
-#define LWIP_STATS                  0
-
-/* MSS should match the hardware packet size */
-#define TCP_MSS                     1460
-#define TCP_SND_BUF                 (2 * TCP_MSS)
-#define TCP_WND                     (2 * TCP_MSS)
-//#define TCP_SND_QUEUELEN            (2 * TCP_SND_BUF/TCP_MSS)
-
-// Broadcast
-#define IP_SOF_BROADCAST            1
-#define IP_SOF_BROADCAST_RECV       1
-
-#define LWIP_BROADCAST_PING         1
-
-#define LWIP_CHECKSUM_ON_COPY       0
-
-#define LWIP_NETIF_HOSTNAME         1
-#define LWIP_NETIF_STATUS_CALLBACK  1
-#define LWIP_NETIF_LINK_CALLBACK    1
-
-/* Do not define htonl, htons, ntohl and ntohs. */
 #define LWIP_PREFIX_BYTEORDER_FUNCS
 
-//#define LWIP_DEBUG
-#define UDP_LPC_EMAC                LWIP_DBG_OFF
-#define SYS_DEBUG                   LWIP_DBG_OFF
-#define PPP_DEBUG                   LWIP_DBG_OFF
-#define IP_DEBUG                    LWIP_DBG_OFF
-#define MEM_DEBUG                   LWIP_DBG_OFF
-#define MEMP_DEBUG                  LWIP_DBG_OFF
-#define PBUF_DEBUG                  LWIP_DBG_OFF
-#define API_LIB_DEBUG               LWIP_DBG_OFF
-#define API_MSG_DEBUG               LWIP_DBG_OFF
-#define TCPIP_DEBUG                 LWIP_DBG_OFF
-#define SOCKETS_DEBUG               LWIP_DBG_OFF
-#define TCP_DEBUG                   LWIP_DBG_OFF
-#define TCP_INPUT_DEBUG             LWIP_DBG_OFF
-#define TCP_FR_DEBUG                LWIP_DBG_OFF
-#define TCP_RTO_DEBUG               LWIP_DBG_OFF
-#define TCP_CWND_DEBUG              LWIP_DBG_OFF
-#define TCP_WND_DEBUG               LWIP_DBG_OFF
-#define TCP_OUTPUT_DEBUG            LWIP_DBG_OFF
-#define TCP_RST_DEBUG               LWIP_DBG_OFF
-#define TCP_QLEN_DEBUG              LWIP_DBG_OFF
-#define ETHARP_DEBUG                LWIP_DBG_OFF
-#define NETIF_DEBUG                 LWIP_DBG_OFF
-#define DHCP_DEBUG                  LWIP_DBG_OFF
+/* Architecture defiens that the user hopefully does not have to
+ * change, but may redefine. */
 
-#endif /* LWIPOPTS_H_ */
+#ifndef TCPIP_THREAD_NAME
+#    define TCPIP_THREAD_NAME           "tcpip"
+#endif
+
+#ifndef TCPIP_THREAD_STACKSIZE
+#    define TCPIP_THREAD_STACKSIZE      1024
+#endif
+
+#ifndef TCPIP_MBOX_SIZE
+#    define TCPIP_MBOX_SIZE             8
+#endif
+
+#ifndef MEM_ALIGNMENT
+#    define MEM_ALIGNMENT               4
+#endif
+
+#ifndef LWIP_RAND
+#    define LWIP_RAND()                 rand()
+#endif
+
+#ifndef LWIP_CHECKSUM_ON_COPY
+#    define LWIP_CHECKSUM_ON_COPY       0
+#endif
+
+/* TCP/IP stack configuration. */
+
+#ifndef LWIP_RAW
+#    define LWIP_RAW                    0
+#endif
+
+#ifndef DEFAULT_TCP_RECVMBOX_SIZE
+#    define DEFAULT_TCP_RECVMBOX_SIZE   8
+#endif
+
+#ifndef DEFAULT_UDP_RECVMBOX_SIZE
+#    define DEFAULT_UDP_RECVMBOX_SIZE   8
+#endif
+
+#ifndef DEFAULT_RAW_RECVMBOX_SIZE
+#    define DEFAULT_RAW_RECVMBOX_SIZE   8
+#endif
+
+#ifndef DEFAULT_ACCEPTMBOX_SIZE
+#    define DEFAULT_ACCEPTMBOX_SIZE     8
+#endif
+
+#ifndef MEM_SIZE
+#    define MEM_SIZE                    8000
+#endif
+
+#ifndef MEMP_NUM_SYS_TIMEOUT
+#    define MEMP_NUM_SYS_TIMEOUT        16
+#endif
+
+#ifndef PBUF_POOL_SIZE
+#    define PBUF_POOL_SIZE              8
+#endif
+
+#ifndef MEMP_NUM_TCP_PCB_LISTEN
+#    define MEMP_NUM_TCP_PCB_LISTEN     4
+#endif
+
+#ifndef MEMP_NUM_TCP_PCB
+#    define MEMP_NUM_TCP_PCB            4
+#endif
+
+#ifndef MEMP_NUM_UDP_PCB
+#    define MEMP_NUM_UDP_PCB            4
+#endif
+
+#ifndef MEMP_NUM_PBUF
+#    define MEMP_NUM_PBUF               8
+#endif
+
+#ifndef MEMP_NUM_NETBUF
+#    define MEMP_NUM_NETBUF             8
+#endif
+
+#ifndef TCP_QUEUE_OOSEQ
+#    define TCP_QUEUE_OOSEQ             0
+#endif
+
+#ifndef TCP_OVERSIZE
+#    define TCP_OVERSIZE                0
+#endif
+
+#ifndef LWIP_DHCP
+#    define LWIP_DHCP                   1
+#endif
+
+#ifndef LWIP_DNS
+#    define LWIP_DNS                    1
+#endif
+
+#ifndef LWIP_IGMP
+#    define LWIP_IGMP                   1
+#endif
+
+#ifndef LWIP_SO_RCVTIMEO
+#    define LWIP_SO_RCVTIMEO            1
+#endif
+
+#ifndef LWIP_TCP_KEEPALIVE
+#    define LWIP_TCP_KEEPALIVE          1
+#endif
+
+#ifndef TCP_MSS
+#    define TCP_MSS                     1460
+#endif
+
+#ifndef TCP_SND_BUF
+#    define TCP_SND_BUF                 (2 * TCP_MSS)
+#endif
+
+#ifndef TCP_WND
+#    define TCP_WND                     (2 * TCP_MSS)
+#endif
+
+#ifndef IP_SOF_BROADCAST
+#    define IP_SOF_BROADCAST            1
+#endif
+
+#ifndef IP_SOF_BROADCAST_RECV
+#    define IP_SOF_BROADCAST_RECV       1
+#endif
+
+#ifndef LWIP_BROADCAST_PING
+#    define LWIP_BROADCAST_PING         1
+#endif
+
+#ifndef LWIP_NETIF_HOSTNAME
+#    define LWIP_NETIF_HOSTNAME         1
+#endif
+
+#ifndef LWIP_NETIF_STATUS_CALLBACK
+#    define LWIP_NETIF_STATUS_CALLBACK  1
+#endif
+
+#ifndef LWIP_NETIF_LINK_CALLBACK
+#    define LWIP_NETIF_LINK_CALLBACK    1
+#endif
+
+
+/* Debugging. */
+
+#ifndef LWIP_NOASSERT
+#    define LWIP_NOASSERT               1
+#endif
+
+#ifndef LWIP_STATS
+#    define LWIP_STATS                  0
+#endif
+
+//#define LWIP_DEBUG
+
+#ifndef UDP_LPC_EMAC
+#    define UDP_LPC_EMAC                LWIP_DBG_OFF
+#endif
+
+#ifndef SYS_DEBUG
+#    define SYS_DEBUG                   LWIP_DBG_OFF
+#endif
+
+#ifndef PPP_DEBUG
+#    define PPP_DEBUG                   LWIP_DBG_OFF
+#endif
+
+#ifndef IP_DEBUG
+#    define IP_DEBUG                    LWIP_DBG_OFF
+#endif
+
+#ifndef MEM_DEBUG
+#    define MEM_DEBUG                   LWIP_DBG_OFF
+#endif
+
+#ifndef MEMP_DEBUG
+#    define MEMP_DEBUG                  LWIP_DBG_OFF
+#endif
+
+#ifndef PBUF_DEBUG
+#    define PBUF_DEBUG                  LWIP_DBG_OFF
+#endif
+
+#ifndef API_LIB_DEBUG
+#    define API_LIB_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef API_MSG_DEBUG
+#    define API_MSG_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef TCPIP_DEBUG
+#    define TCPIP_DEBUG                 LWIP_DBG_OFF
+#endif
+
+#ifndef SOCKETS_DEBUG
+#    define SOCKETS_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_DEBUG
+#    define TCP_DEBUG                   LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_INPUT_DEBUG
+#    define TCP_INPUT_DEBUG             LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_FR_DEBUG
+#    define TCP_FR_DEBUG                LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_RTO_DEBUG
+#    define TCP_RTO_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_CWND_DEBUG
+#    define TCP_CWND_DEBUG              LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_WND_DEBUG
+#    define TCP_WND_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_OUTPUT_DEBUG
+#    define TCP_OUTPUT_DEBUG            LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_RST_DEBUG
+#    define TCP_RST_DEBUG               LWIP_DBG_OFF
+#endif
+
+#ifndef TCP_QLEN_DEBUG
+#    define TCP_QLEN_DEBUG              LWIP_DBG_OFF
+#endif
+
+#ifndef ETHARP_DEBUG
+#    define ETHARP_DEBUG                LWIP_DBG_OFF
+#endif
+
+#ifndef NETIF_DEBUG
+#    define NETIF_DEBUG                 LWIP_DBG_OFF
+#endif
+
+#ifndef DHCP_DEBUG
+#    define DHCP_DEBUG                  LWIP_DBG_OFF
+#endif
+
+#endif
