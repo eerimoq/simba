c5029bd14eac70a51bd47524d778d3614b55922d
Erik Moqvist <erik.moqvist@gmail.com>
2016-07-18 21:09:53 +0200

    Added retries of software upload to the Photon.

--- src/boards/photon/board.py
+++ src/boards/photon/board.py
@@ -2,18 +2,19 @@
 
 from __future__ import print_function
 
-import serial
+import sys
 import subprocess
 import time
 import argparse
+import serial
 
 
-def subcommand_upload(args):
+def upload(port, binary):
     """Upload the binary to the board.
 
     """
 
-    dev_serial = serial.Serial("/dev/" + args.port)
+    dev_serial = serial.Serial("/dev/" + port)
 
     # 1. Hold down the RESET and SETUP buttons.
     dev_serial.dtr = 1
@@ -38,12 +39,35 @@ def subcommand_upload(args):
         "--device", "2b04:d006",
         "--alt", "0",
         "--dfuse-address", "0x08020000:leave",
-        "--download", args.binary
+        "--download", binary
     ]
 
     print(' '.join(command))
 
-    subprocess.check_call(command)
+    try:
+        subprocess.check_call(command)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def subcommand_upload(args):
+    """File upload subcommand.
+
+    """
+
+    attempt = 1
+
+    while attempt <= 3:
+        print("Uploading the software attempt {}/3.".format(attempt))
+
+        if upload(args.port, args.binary):
+            break
+
+        attempt += 1
+    else:
+        print("Failed to upload {} to the Photon.".format(args.binary))
+        sys.exit(1)
 
 
 def main():
