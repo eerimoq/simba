f2a11a2d67e6d06b3bfd40cb00b25de5fac25f9a
Erik Moqvist <erik.moqvist@gmail.com>
2016-07-27 23:51:20 +0200

    Fix: json test suite on STM32. Stack overflow.

--- tst/encode/json/main.c
+++ tst/encode/json/main.c
@@ -146,8 +146,8 @@ static int encode(const char *r_p,
     int ok = 1;
     va_list ap;
     struct json_t p;
-    static char buf[512];
-    static struct json_tok_t t[128];
+    char buf[512];
+    struct json_tok_t t[128];
 
     va_start(ap, numtok);
 
@@ -200,7 +200,7 @@ static int decode(const char *s_p,
     int ok = 1;
     va_list args;
     struct json_t p;
-    static struct json_tok_t t[128];
+    struct json_tok_t t[128];
 
     if (numtok > 128) {
         return (0);
