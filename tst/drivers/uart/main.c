c5c508c3e32df08dcbabc98476dfcfd5154c1280
Erik Moqvist <erik.moqvist@gmail.com>
2017-01-14 14:48:33 +0100

    Rewritten UART test suite.

--- tst/drivers/uart/main.c
+++ tst/drivers/uart/main.c
@@ -2,9 +2,9 @@
  * @section License
  *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2014-2016, Erik Moqvist
- * 
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -30,25 +30,68 @@
 
 #include "simba.h"
 
-static char stat[128];
-static char buf[32];
+static struct uart_driver_t uarts[UART_DEVICE_MAX - 1];
+static char rxbufs[UART_DEVICE_MAX - 1][8];
+
+static int test_echo_server(struct harness_t *harness_p)
+{
+    struct uart_driver_t *uart_p;
+    char byte;
+    int i;
+    struct chan_list_t list;
+    void *workspace[UART_DEVICE_MAX];
+
+    chan_list_init(&list, workspace, sizeof(workspace));
+
+    /* Initialize and start all UART drivers except the UART used for
+       the console. */
+    for (i = 0; i < membersof(uarts); i++) {
+        uart_init(&uarts[i],
+                  &uart_device[i + 1],
+                  38400,
+                  &rxbufs[i][0],
+                  sizeof(rxbufs[0]));
+        uart_start(&uarts[i]);
+        chan_list_add(&list, &uarts[i]);
+    }
+
+    std_printf(FSTR("echo server testing UART 1-%d press 'q' to quit\r\n"),
+               UART_DEVICE_MAX - 1);
+
+    while (1) {
+        uart_p = chan_list_poll(&list, NULL);
+        uart_read(uart_p, &byte, sizeof(byte));
+
+        std_printf(FSTR("Read 0x%x on UART %d.\r\n"),
+                   (int)byte,
+                   (uart_p - &uarts[0]) / sizeof(uarts[0]) + 1);
+
+        if (byte == 'q') {
+            break;
+        }
+
+        uart_write(uart_p, &byte, sizeof(byte));
+    }
+
+    for (i = 0; i < membersof(uarts); i++) {
+        uart_stop(&uarts[i]);
+    }
+
+    return (0);
+}
 
 int main()
 {
-    struct uart_driver_t uart;
-    uint8_t flags = 3;
+    struct harness_t harness;
+    struct harness_testcase_t harness_testcases[] = {
+        { test_echo_server, "test_echo_server" },
+        { NULL, NULL }
+    };
 
     sys_start();
-    uart_module_init();
 
-    uart_init(&uart, &uart_device[0], 38400, buf, sizeof(buf));
-    uart_start(&uart);
-
-    uart_write(&uart, stat, sprintf(stat, "stat: %x", flags));
-
-    while (1);
-
-    uart_stop(&uart);
+    harness_init(&harness);
+    harness_run(&harness, harness_testcases);
 
     return (0);
 }
