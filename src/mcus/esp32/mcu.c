f609155090c83dea64d0cf00e3f4ae54a35a5e80
Erik Moqvist <erik.moqvist@gmail.com>
2017-07-02 12:45:35 +0200

    ESP32 software I2C pins 21 (SDA) and 22 (SCL).

--- src/mcus/esp32/mcu.c
+++ src/mcus/esp32/mcu.c
@@ -238,8 +238,8 @@ struct dac_device_t dac_device[DAC_DEVICE_MAX];
 /* Use software i2c. */
 struct i2c_device_t i2c_device[I2C_DEVICE_MAX] = {
     {
-        .scl_p = &pin_device[0],
-        .sda_p = &pin_device[1],
+        .scl_p = &pin_device[22],
+        .sda_p = &pin_device[21],
         .max_clock_stretching_us = 100000,
         .clock_stretching_sleep_us = 1000
     }
